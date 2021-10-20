9f191c39fcbeac14cdb79a126da28822c70cc9db
Aleks Marjanovic <alex.marjanovic11@gmail.com>
2021-10-20 14:28:57 -0400

    Finishes the assignment

--- asgn07-CRUD/chain_gang/private/classes/bicycle.class.php
+++ asgn07-CRUD/chain_gang/private/classes/bicycle.class.php
@@ -1,107 +1,9 @@
 <?php
 
-class Bicycle {
+class Bicycle extends DatabaseObject{
 
-  // ----- START OF ACTIVE RECORD CODE ------
-  static protected $database;
+  static protected $table_name = 'bicycles';
   static protected $db_columns = ['id', 'brand', 'model', 'year', 'category', 'color', 'gender', 'price', 'weight_kg', 'condition_id', 'description'];
-
-  static public function set_database($database) {
-    self::$database = $database;
-  }
-
-  static public function find_by_sql($sql) {
-    $result = self::$database->query($sql);
-    if(!$result) {
-      exit("Database query failed.");
-    }
-
-    // results into objects
-    $object_array = [];
-    while($record = $result->fetch_assoc()) {
-      $object_array[] = self::instantiate($record);
-    }
-
-    $result->free();
-
-    return $object_array;
-  }
-
-  static public function find_all() {
-    $sql = "SELECT * FROM bicycles";
-    return self::find_by_sql($sql);
-  }
-
-  static public function find_by_id($id) {
-    $sql = "SELECT * FROM bicycles ";
-    $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
-    $obj_array = self::find_by_sql($sql);
-    if(!empty($obj_array)) {
-      return array_shift($obj_array);
-    } else {
-      return false;
-    }
-  }
-
-  static protected function instantiate($record) {
-    $object = new self;
-    // Could manually assign values to properties
-    // but automatically assignment is easier and re-usable
-    foreach($record as $property => $value) {
-      if(property_exists($object, $property)) {
-        $object->$property = $value;
-      }
-    }
-    return $object;
-  }
-
-  /**
-   * Instance method which creates a new database object based on the input
-   *
-   * @return boolean 
-   */
-  public function create() {
-    $attributes = $this->sanitized_attributes();
-    $sql = "INSERT INTO bicycles (";
-    $sql .= join(', ', array_keys($attributes));
-    $sql .= ") VALUES ('";
-    $sql .= join("', '", array_values($attributes));
-    $sql .= "')";
-    $result = self::$database->query($sql);
-    if($result) {
-      $this->id = self::$database->insert_id;
-    }
-    return $result;
-  }
-
-  /**
-   * Creates an array of attributes in a form of key-value pair
-   *
-   * @return array
-   */
-  public function attributes() {
-    $attributes = [];
-    foreach(self::$db_columns as $column) {
-      if($column == 'id') {continue;}
-      $attributes[$column] = $this->$column;
-    }
-    return $attributes;
-  }
-
-  /**
-   * Takes the values from the attributes array and sanitizes them to prevent SQL injection
-   *
-   * @return array
-   */
-  protected function sanitized_attributes() {
-    $sanitized = [];
-    foreach($this->attributes() as $key => $value) {
-      $sanitized[$key] = self::$database->escape_string($value);
-    }
-    return $sanitized;
-  }
-
-  // ----- END OF ACTIVE RECORD CODE ------
 
   public $id;
   public $brand;
@@ -174,6 +76,22 @@ class Bicycle {
     } else {
       return "Unknown";
     }
+  }
+
+    /**
+   * Checks whether there were any input errors and displays the according error message if there were
+   *
+   * @return void
+   */
+  protected function validate() {
+    $this->errors = [];
+    if(is_blank($this->brand)) {
+      $this->errors[] = "Brand cannot be blank.";
+    }
+    if(is_blank($this->model)) {
+      $this->errors[] = "Model cannot be blank.";
+    }
+    return $this->errors;
   }
 
 }
---
+++ asgn07-CRUD/index.php
@@ -0,0 +1,3 @@
+<?php
+require_once('private/initialize.php'); 
+var_dump(redirect_to('chain_gang/public/staff/index.php'));
