554431be1f3be42c0287453fc7e03f475ed88b4b
Aleks Marjanovic <alex.marjanovic11@gmail.com>
2021-10-25 20:19:42 -0400

    Sets up the files for the Asgn08

---
+++ asgn08-bird-CRUD/index.php
@@ -0,0 +1,3 @@
+<?php
+require_once('private/initialize.php'); 
+var_dump(redirect_to('public/index.php'));
---
+++ asgn08-bird-CRUD/private/classes/bird.class.php
@@ -0,0 +1,39 @@
+<?php
+
+class Bird {
+  public $common_name;
+  public $habitat;
+  public $food;
+  public $nest_placement;
+  public $behavior;
+  public $conservation_id;
+  public $backyard_tips;
+
+  public const CONSERVATION = [ 
+    1 => 'low',
+    2 => 'moderate',
+    3 => 'high',
+    4 => 'extreme'
+  ];
+
+  public function __construct($args=[]) {
+    $this->common_name = $args['common_name'] ?? '';
+    $this->habitat = $args['habitat'] ?? '';
+    $this->food = $args['food'] ?? '';
+    $this->nest_placement = $args['nest_placement'] ?? '';
+    $this->behavior = $args['behavior'] ?? '';
+    $this->conservation_id = $args['conservation_id'] ?? 1;
+    $this->backyard_tips = $args['backyard_tips'] ?? '';
+  }
+
+  public function conservation_level() {
+    if($this->conservation_id > 0) {
+      return self::CONSERVATION[$this->conservation_id];
+    } else {
+      return 'Unknown';
+    }
+  }
+
+}
+
+?>
